6a8f69b0985981798af71cea5a4d434db3577031
Rohan Nunugonda <rohannunu@gmail.com>
2025-11-16 16:34:10 -0600

    emit stats from lru cache

--- lru-cache/lru_cache.go
+++ lru-cache/lru_cache.go
@@ -2,15 +2,43 @@ package lrucache
 
 import (
 	"container/list"
+	"sync/atomic"
 
 	"github.com/rohannunu/pebble-cs598rap/cache"
 )
+
+type LRUStats struct {
+	Hits       uint64
+	Misses     uint64
+	Promotions uint64
+	Evictions  uint64
+}
 
 type LRUCache struct {
 	cache    *cache.Cache
 	order    *list.List
 	elements map[string]*list.Element
 	capacity int
+	stats    *LRUStats
+}
+
+func (s *LRUStats) RecordHit()     { atomic.AddUint64(&s.Hits, 1) }
+func (s *LRUStats) RecordMiss()    { atomic.AddUint64(&s.Misses, 1) }
+func (s *LRUStats) RecordPromote() { atomic.AddUint64(&s.Promotions, 1) }
+func (s *LRUStats) RecordEvict()   { atomic.AddUint64(&s.Evictions, 1) }
+func (s *LRUStats) Snapshot() LRUStats {
+	return LRUStats{
+		Hits:       atomic.LoadUint64(&s.Hits),
+		Misses:     atomic.LoadUint64(&s.Misses),
+		Promotions: atomic.LoadUint64(&s.Promotions),
+		Evictions:  atomic.LoadUint64(&s.Evictions),
+	}
+}
+
+
+
+func (lru *LRUCache) Stats() LRUStats {
+	return lru.stats.Snapshot()
 }
 
 func NewLRUCache(capacity int) *LRUCache {
@@ -19,6 +47,7 @@ func NewLRUCache(capacity int) *LRUCache {
 		order:    list.New(),
 		elements: make(map[string]*list.Element),
 		capacity: capacity,
+		stats:    &LRUStats{},
 	}
 }
 
@@ -41,6 +70,8 @@ func (lru *LRUCache) Get(key []byte) ([]byte, bool, error) {
 		return nil, false, err
 	}
 	if exists {
+		//we have hit in the cache
+		lru.stats.RecordHit()
 		value, found, err := lru.cache.Get(key)
 		if err != nil {
 			return nil, false, err
@@ -50,6 +81,9 @@ func (lru *LRUCache) Get(key []byte) ([]byte, bool, error) {
 			return value, true, nil
 		}
 	} else {
+		//we have a miss :(
+		lru.stats.RecordMiss()
+
 		// if its not, try to get it from pebble directly and promote it to the cache
 		value, found, err := lru.cache.Get(key)
 		if err != nil {
@@ -57,6 +91,7 @@ func (lru *LRUCache) Get(key []byte) ([]byte, bool, error) {
 		}
 		if found {
 			// promote to cache
+			lru.stats.RecordPromote()
 			_, err := lru.Set(key, value, true)
 			if err != nil {
 				return nil, false, err
@@ -79,6 +114,7 @@ func (lru *LRUCache) Set(key []byte, value []byte, to_cache bool) (bool, error) 
 	if lru.order.Len() >= lru.capacity && to_cache {
 		backElem := lru.order.Back()
 		if backElem != nil {
+			lru.stats.RecordEvict()
 			_, err := lru.cache.Evict([]byte(backElem.Value.(string)))
 			if err != nil {
 				return false, err
